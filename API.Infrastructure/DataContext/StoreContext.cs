0a32288dda64a04f0f44d9f304db67213a6ec818
Emre Gulten <emre.gulten035@gmail.com>
2021-04-18 15:50:40 +0300

    Mimari Kuruldu

---
+++ API.Infrastructure/DataContext/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using API.Core.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Infrastructure.DataContext
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions db):base(db)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
