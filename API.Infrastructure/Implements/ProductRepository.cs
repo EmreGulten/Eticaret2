55a22883d2ee4c8585bbcaf648d97743c0c3d5a1
Emre Gulten <emre.gulten035@gmail.com>
2021-04-24 02:25:33 +0300

    Entityler oluşturuldu

---
+++ API.Infrastructure/Implements/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using API.Core.DbModels;
+using API.Core.Interfaces;
+using API.Infrastructure.DataContext;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.Implements
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _context;
+
+        public ProductRepository(StoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<IReadOnlyList<Product>> GetProductAsync()
+        {
+            return await _context.Products
+                .Include(x => x.ProductBrands)
+                .Include(x => x.ProductType)
+                .ToListAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _context.Products
+                .Include(x => x.ProductBrands)
+                .Include(x => x.ProductType)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            return await _context.ProductBrands.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
+        }
+    }
+}
